363150291ed23d9f28ef2d0110d354769ba253b3
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-08-10 11:58:23 +0530

    Create Image Optimizations

--- image/sample_image_project/src/app/page.jsx
+++ image/sample_image_project/src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import img1 from "../../public/wallhaven-jxoxjm.jpg"
 
 export default function Home() {
   return (
@@ -7,6 +8,15 @@ export default function Home() {
       <h1>
         Image Page
       </h1>
+
+      {/* <img src={img1.src} /> */}
+
+      <Image 
+      src={img1}
+      alt="Sample Image"
+      style={{width: "100%", objectFit: "cover"}}
+      />
+
     </div>
   );
 }
